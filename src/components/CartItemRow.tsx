8b0564f9bc6c44e531cd7eb7921f99be1297ae03
Mmadiba724 <matendehussein@gmail.com>
2025-12-29 21:27:35 +0300

    depoly set

--- src/components/CartItemRow.tsx
+++ src/components/CartItemRow.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import type { CartItem } from "../context/CartContext";
 
 export default function CartItemRow({
--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,5 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  base: '/children_books/',
 })
