1f5da00ee1d50cf38b7de30d9bc0905641864b55
Mmadiba724 <matendehussein@gmail.com>
2026-03-11 15:50:33 +0300

    feat: Set basename for BrowserRouter to support deployment on GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -40,7 +40,7 @@ normalizeGitHubPagesHashRedirect();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
             <App />
         </BrowserRouter>
     </React.StrictMode>,
